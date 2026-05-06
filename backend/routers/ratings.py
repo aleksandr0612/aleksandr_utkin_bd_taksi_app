8384d1d871df48ea87cc4352f9dd5ca41bc230ee
aleksandr0612 <106372150+aleksandr0612@users.noreply.github.com>
2026-05-06 20:50:34 +0300

    Delete backend directory

--- backend/routers/ratings.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/ratings", tags=["Ratings"])
-
-@router.get("/")
-def get_ratings():
-    return {"message": "Ratings endpoint (rezervuota plėtrai)"}
